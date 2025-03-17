e3f6efa9c82b5759fda3d7993cb1c9b4a30d2346
banasu0723 <banasu0723@gmail.com>
2025-03-17 11:09:58 +0900

    최대힙 / 40m

---
+++ week1/datastructure/최대힙/banasu0723/Main.java
@@ -0,0 +1,30 @@
+package datastructure.최대힙.banasu0723;
+
+import java.io.IOException;
+import java.util.*;
+
+// 최대힙은 우선순위 큐 자료구조를 사용
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int x;
+        // 우선 순위 큐 - 내림차순 정렬
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder(); // StringBuilder 객체 생성 - 이걸로 출력하면 성능 향상
+
+        for(int i=0; i<N; i++){
+            x = sc.nextInt();
+            if(x>0){
+                pq.offer(x);
+            }else if(x==0){
+                if(pq.isEmpty()){
+                    sb.append(0).append("\n");
+                }else{
+                    sb.append(pq.poll()).append("\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
