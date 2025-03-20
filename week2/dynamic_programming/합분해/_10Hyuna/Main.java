828a990a42fa9a97060a8242f1d117b31e1c37de
10Hyuna <kimha105@naver.com>
2025-03-20 15:20:58 +0900

    돌게임 / 5m

---
+++ week2/dynamic_programming/합분해/_10Hyuna/Main.java
@@ -0,0 +1,14 @@
+package week2.dynamic_programming.합분해._10Hyuna;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+    }
+
+}
