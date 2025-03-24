d89a3442eb3dd827577ca0ebc837ba1862dc733b
banasu0723 <banasu0723@gmail.com>
2025-03-24 14:39:00 +0900

    돌게임 Scanner에서 BufferedReader 로 수정

--- week2/dynamic_programming/돌게임/banasu0723/Main.java
+++ week2/dynamic_programming/돌게임/banasu0723/Main.java
@@ -1,11 +1,15 @@
 package dynamic_programming.돌게임.banasu0723;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
         int winner = 0;
         while(N!=0){
             if(N>3){
