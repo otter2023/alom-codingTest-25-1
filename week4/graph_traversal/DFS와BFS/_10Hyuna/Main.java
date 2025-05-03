521021c344d1aadf7d4f5035a052ec98cf731db1
10Hyuna <kimhaa1005@naver.com>
2025-05-04 01:49:23 +0900

    DFS와 BFS 1H

---
+++ week4/graph_traversal/DFS와BFS/_10Hyuna/Main.java
@@ -0,0 +1,79 @@
+package week4.graph_traversal.DFS와BFS._10Hyuna;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] check;
+    static int[][] arr;
+
+    static int n, m, v;
+
+    static Queue<Integer> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
+
+        arr = new int[n + 1][n + 1];
+        check = new boolean[n + 1];
+
+        int a, b;
+
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = 1;
+            arr[b][a] = 1;
+        }
+
+        dfs(v);
+        sb.append("\n");
+        check = new boolean[n +1];
+
+        bfs(v);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int start) {
+        check[start] = true;
+        sb.append(start + " ");
+
+        for(int i = 0; i <= n; i++) {
+            if(arr[start][i] == 1 && !check[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+    public static void bfs(int start) {
+        q.add(start);
+        check[start] = true;
+
+        while(!q.isEmpty()) {
+            start = q.poll();
+            sb.append(start + " ");
+
+            for(int i = 1; i <= n; i++) {
+                if(arr[start][i] == 1 && !check[i]) {
+                    q.add(i);
+                    check[i] = true;
+                }
+            }
+        }
+    }
+
+}
